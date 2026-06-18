10496ad2dc245cccbe050136f5a777956ce76f1c
hoanglong212 <69975681+hoanglong212@users.noreply.github.com>
2026-06-18 11:02:36 +0700

    chore: scaffold Laravel API backend

---
+++ backend/database/seeders/IncidentCategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IncidentCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
